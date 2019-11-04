059c3dde922f1712ae52d8eaebd05d96b2e8961c
vmejiaec <vmejiaec@gmail.com>
2019-11-04 15:50:26 -0500

    Continúo implementando el editor

--- js/TuringEmul.js
+++ js/TuringEmul.js
@@ -6,7 +6,8 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"),
     mode: "javascript",
     styleActiveLine: true,
     lineNumbers: true,
-    gutters: ["CodeMirror-linenumbers", "breakpoints"]
+    gutters: ["CodeMirror-linenumbers", "breakpoints"],
+    extraKeys: {"Alt-F": "findPersistent"}
     }
 );
 
