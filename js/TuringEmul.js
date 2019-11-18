be0d3b50db0f98c7c092c3680917d452c091ae1e
vmejiaec <vmejiaec@gmail.com>
2019-11-18 06:05:39 -0500

    mejoras

--- js/TuringEmul.js
+++ js/TuringEmul.js
@@ -47,6 +47,24 @@ function publicarTM(tm){
     document.getElementById("TPosEdit").value = tm.posCabeza;
 }
 
+function publicarTMsinCodigo(tm){
+    // TODO 
+    // Debería provenir de la clase TM con un HTML preformateado listo para salir a la página web
+    document.getElementById("TCinta").innerHTML = tm.cintaHtml();
+    document.getElementById("TEstado").innerText = tm.estado;
+    document.getElementById("TParada").innerText = tm.parada;
+    document.getElementById("TPos").innerText = tm.posCabeza;
+    document.getElementById("TLinea").innerText = tm.lineaEjec;
+    //editor.setValue(tm.code);
+    if (!tm.parada) 
+        highlightLine(tm.lineaEjec);
+
+    // Publico en los campos de edición
+    document.getElementById("TCintaEdit").value = tm.cinta;
+    document.getElementById("TEstadoEdit").value = tm.estado;
+    document.getElementById("TPosEdit").value = tm.posCabeza;
+}
+
 function capturarTM(tm){
     tm.cinta = document.getElementById("TCintaEdit").value ;
     tm.estado = document.getElementById("TEstadoEdit").value;
@@ -63,9 +81,15 @@ function carga(){
 function run(){
     capturarTM(tm_actual);
     var seEjecuto = tm_actual.ejecUnaVez();
-    publicarTM(tm_actual);
+    publicarTMsinCodigo(tm_actual);
 }
 
 function marcar(linea){
     editor.markText({line: linea, ch: 0}, {line: linea, ch: 100}, {className: "styled-background"});
+}
+
+function highlightLine(lineNumber) {
+    var editorC = editor.CodeMirror;
+    //Set line css class
+    editor.addLineClass(lineNumber, 'background', 'TLineaActual');
 }
